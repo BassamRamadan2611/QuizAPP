561d741bc124c8e4a24767055e2cd33d0f072253
bassam ramadan <bassamramadan964@gmail.com>
2023-08-12 14:14:09 +0300

    update db

--- src/app/Auth/services/auth.service.ts
+++ src/app/Auth/services/auth.service.ts
@@ -10,29 +10,27 @@ import { environment } from 'src/environments/environment';
 export class AuthService {
 
 
-
   constructor(private http:HttpClient) { }
-
   user = new Subject()
   createUser(model:any) {
-    return this.http.post('https://quiz-app-api-vvmj-8z44z9phd-bassamramadan2611.vercel.app/students' , model)
+    return this.http.post(environment.baseApi+'students' , model)
   }
 
   login(model:any) {
-    return this.http.put("https://quiz-app-api-vvmj-8z44z9phd-bassamramadan2611.vercel.app/login/1" , model)
+    return this.http.put(environment.baseApi+'login/1' , model)
   }
 
   getUsers(type:string){
-    return this.http.get("https://quiz-app-api-vvmj-8z44z9phd-bassamramadan2611.vercel.app/"+type)
+    return this.http.get(environment.baseApi+type)
   }
 
   getStudent(id:number) {
-    return this.http.get("https://quiz-app-api-vvmj-8z44z9phd-bassamramadan2611.vercel.app/students" +id)
+    return this.http.get(environment.baseApi+"students/"+id)
   }
   updateStudent(id:number , model:any) {
-    return this.http.put("https://quiz-app-api-vvmj-8z44z9phd-bassamramadan2611.vercel.app/students" +id , model)
+    return this.http.put(environment.baseApi+"students/"+id , model)
   }
   getRole() {
-    return this.http.get("https://quiz-app-api-vvmj-8z44z9phd-bassamramadan2611.vercel.app/login/1")
+    return this.http.get(environment.baseApi+'login/1')
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
- baseApi:"https://quiz-app-api-vvmj-8z44z9phd-bassamramadan2611.vercel.app/",
-
-  production: false
-};
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+    baseApi:"http://localhost:3000/",
+    production: false
+  };
+  
+  /*
+   * For easier debugging in development mode, you can import the following file
+   * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+   *
+   * This import should be commented out in production mode because it will have a negative impact
+   * on performance if an error is thrown.
+   */
+  // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
